0ffc37f714ecd839fccc52dcb449a2c45724c3c2
KrSAdal <adal.ac.ramirez@gmail.com>
2023-06-29 22:40:45 -0600

    Registro 2

---
+++ registro.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    
+</head>
+
+<body>
+    <div class="principal">
+        <div class="barralateral">
+                <img src="" alt="">
+            <div class="barraoculta">
+                <i class="fa fa-times" aria-hidden="true"></i>
+                <img src="https://pm1.aminoapps.com/8402/a81ad5903e36df22e838e3c27f17e95f09cd3cbar1-1070-1637v2_hq.jpg" style="width: 50px;" alt=":(">
+            </div>
+            <ul>
+                <li><a href="#"><label for="inicio">Inicio</label></a></li>
+                <li><a href="#"><label for="inicio">Registrar Estudiante</label></a></li>
+                <li><a href="#"><label for="inicio">Visualizar Listado</label></a></li>
+            </ul>
+        </div>
+
+        <div class="header">
+            <div class="mb-3">
+                <label for="1nombre" class="form-label">Primer Nombre</label>
+                <input type="text" class="form-control" id="1nombre" placeholder="" style="width : 100%">
+            </div>
+            <div class="mb-3">
+                <label for="2nombre" class="form-label">Segundo Nombre</label>
+                <input type="text" class="form-control" id="2nombre" placeholder="" style="width : 100%">
+            </div>
+            <div class="mb-3">
+                <label for="1apellido" class="form-label">Primer Apellido</label>
+                <input type="text" class="form-control" id="1apellido" placeholder="" style="width : 100%">
+            </div>
+            <div class="mb-3">
+                <label for="2apellido" class="form-label">Segundo Apellido</label>
+                <input type="text" class="form-control" id="2apellido" placeholder="" style="width : 100%">
+            </div>
+            <div class="mb-3">
+                <label for="telEstudiante" class="form-label">Telefono Estudiante</label>
+                <input type="number" class="form-control" id="telEstudiante" placeholder="" style="width : 100%">
+            </div>
+            <div class="mb-3">
+                <label for="telPadre" class="form-label">Telefono Padres</label>
+                <input type="number" class="form-control" id="telPadre" placeholder="" style="width : 100%">
+            </div>
+            <div class="mb-3">
+                <label for="jornada" class="form-label">Jornada</label>
+                <input type="text" class="form-control" id="jornada" placeholder="" style="width : 100%">
+            </div>
+            <div class="mb-3">
+                <label for="grado" class="form-label">Grado</label>
+                <input type="text" class="form-control" id="grado" placeholder="" style="width : 100%">
+            </div>
+            <div class="mb-3">
+                <label for="carrera" class="form-label">Carrera</label>
+                <input type="text" class="form-control" id="carrera" placeholder="" style="width : 100%">
+            </div>
+            <div class="footer">
+                <div class="col-auto">
+                    <button type="submit" class="btn btn-primary mb-3">Registrar</button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</body>
+
+</html>
